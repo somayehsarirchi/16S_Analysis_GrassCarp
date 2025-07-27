44faf2cc09ae3262fd2e4125ff4782bc74e8613d
somayehsarirchi <103599812+somayehsarirchi@users.noreply.github.com>
2025-07-27 15:49:16 +0330

    Update make_metadata.sh

--- scripts/make_metadata.sh
+++ scripts/make_metadata.sh
@@ -6,16 +6,16 @@
 OUTFILE="metadata_with_group.tsv"
 
 # Header
-echo -e "#SampleID	group" > $OUTFILE
+echo -e "SampleID\tgroup" > $OUTFILE
 
 # Diet group samples
 for sample in SRR34584690 SRR34584691 SRR34584692 SRR34584693 SRR34584694 SRR34584695 SRR34584696 SRR34584698 SRR34584699; do
-  echo -e "${sample}	Diet" >> $OUTFILE
+    echo -e "${sample}\tDiet" >> $OUTFILE
 done
 
 # Control group samples
 for sample in SRR34584700 SRR34584701 SRR34584697; do
-  echo -e "${sample}	Control" >> $OUTFILE
+    echo -e "${sample}\tControl" >> $OUTFILE
 done
 
 echo "✅ Metadata file created: $OUTFILE"
